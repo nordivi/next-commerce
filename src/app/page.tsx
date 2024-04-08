c9d19b1a3289e3324a3e972aa3021d76c9ffd57a
nordivi <victorw.nordi@icloud.com>
2024-04-08 14:25:26 -0300

    definindo tipagem para produto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,29 @@
-export default function Home() {
+import { ProductType } from "@/types/ProductType";
+
+async function getProducts(){
+  const rest = await fetch ('https://fakestoreapi.com/products')
+
+  if (!rest.ok) {
+    throw new Error('Failed to fetch')
+    
+  }
+
+  return rest.json()
+}
+
+export default async function Home() {
+  const products = await getProducts()
+
+
   return (
     <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0 ">
       <div className="grid grid-cols-1 
       sm: grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 
       xl:gap-6" >
-          <div>Prod 1</div>
-          <div>Prod 1</div>
-          <div>Prod 1</div>
-          <div>Prod 1</div>
-          <div>Prod 1</div>
-
+          {products.map((product: ProductType) =>  (
+            <div key={product.id}> {product.title}</div>
+          ))
+          }
 
       </div>
       <h1>Next E-commerce 13</h1>
