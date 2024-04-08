76b52888aa5e3accdbb577ad37d14fea63ccbd62
nordivi <victorw.nordi@icloud.com>
2024-04-08 14:17:15 -0300

    listagem produtos pt1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,17 @@
 export default function Home() {
   return (
-    <div>
+    <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0 ">
+      <div className="grid grid-cols-1 
+      sm: grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 
+      xl:gap-6" >
+          <div>Prod 1</div>
+          <div>Prod 1</div>
+          <div>Prod 1</div>
+          <div>Prod 1</div>
+          <div>Prod 1</div>
+
+
+      </div>
       <h1>Next E-commerce 13</h1>
     </div>
   );
