2f0f8993aae4bcd554397affb4d95f2f30a2585e
nordivi <victorw.nordi@icloud.com>
2024-04-08 15:19:29 -0300

    Estilizando componente produto com next image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from './components/Navbar'
-
+import clsx from "clsx";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -17,9 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={clsx(inter.className, 'bg-slate-700')}>
         <NavBar/>
-        <main className='bg-slate-700 h-screen p-16'>
+        <main className='h-screen p-16'>
             {children}
           </main>
         
