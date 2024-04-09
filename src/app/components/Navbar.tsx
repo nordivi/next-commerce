d98fc907760d5f9042bbb966181c356b52d682a3
nordivi <victorw.nordi@icloud.com>
2024-04-09 13:58:51 -0300

    cart

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -9,7 +9,17 @@ function NavBar() {
             Next Store
           </Link>
           <div className="flex items-center gap-8">
-            <SignedIn>
+            <div className="flex items-center cursor-pointer relative">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" id="Cart" className="h-8 w-8  fill-current text-white">
+              <path d="M14 36c-2.21 0-3.98 1.79-3.98 4s1.77 4 3.98 4 4-1.79 4-4-1.79-4-4-4zM2 4v4h4l7.19 15.17-2.7 4.9c-.31.58-.49 1.23-.49 1.93 0 2.21 1.79 4 4 4h24v-4H14.85c-.28 0-.5-.22-.5-.5 0-.09.02-.17.06-.24L16.2 26h14.9c1.5 0 2.81-.83 3.5-2.06l7.15-12.98c.16-.28.25-.61.25-.96a2 2 0 0 0-2-2H10.43l-1.9-4H2zm32 32c-2.21 0-3.98 1.79-3.98 4s1.77 4 3.98 4 4-1.79 4-4-1.79-4-4-4z" fill="#ffffff">
+              </path><path fill="none" d="M0 0h48v48H0z"></path></svg>
+              <span className="bg-teal-700 text-sm text-bold rounded-full h-5 w-5 text-center flex items-center justify-center absolute top-5 ">
+                  2
+              </span>
+
+            </div>
+            <div>
+              <SignedIn>
               <UserButton/>
             </SignedIn>
             <SignedOut>
@@ -19,6 +29,8 @@ function NavBar() {
               </button>
               </SignInButton>
             </SignedOut>
+            </div>
+            
           </div>
         </nav>
     )
--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -11,7 +11,7 @@ export default function Product(){
         <SkeletonCard isLoading/>
         <SkeletonCard isLoading/>
         <SkeletonCard isLoading/>
-        <SkeletonCard isLoading/>
+        <SkeletonCard isLoading/> 
         <SkeletonCard isLoading/>
         <SkeletonCard isLoading/>
         <SkeletonCard isLoading/>
